cbe32018bb0513b70c31802ba2da650933057837
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-03-26 12:56:51 -0400

    src/main.rs
    diff --git a/src/main.rs b/src/main.rs
    index 98afb58..3e275a9 100644
    --- a/src/main.rs
    +++ b/src/main.rs
    @@ -1,3 +1,16 @@
    +#![warn(clippy::nursery, clippy::pedantic)]
    +#![allow(
    +    clippy::cast_possible_truncation,
    +    clippy::cast_sign_loss,
    +    clippy::cast_precision_loss,
    +    clippy::module_name_repetitions,
    +    clippy::struct_excessive_bools,
    +    clippy::unused_self,
    +    clippy::future_not_send
    +)]
    +
    +use clap::{Arg, ArgAction, Command, Parser};
    +
     mod ui;
     mod app;
     mod cube;

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+#![warn(clippy::nursery, clippy::pedantic)]
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss,
+    clippy::cast_precision_loss,
+    clippy::module_name_repetitions,
+    clippy::struct_excessive_bools,
+    clippy::unused_self,
+    clippy::future_not_send
+)]
+
+use clap::{Arg, ArgAction, Command, Parser};
+
 mod ui;
 mod app;
 mod cube;
+mod global_rt;
 use crossterm::{
     event::{DisableMouseCapture, EnableMouseCapture},
     execute,
