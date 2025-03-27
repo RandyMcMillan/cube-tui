131d15a506beb968cc1ae91f28678a45a856235f
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-03-26 20:10:48 -0400

    src/system_command.rs:dirty commit

--- src/system_command.rs
+++ src/system_command.rs
@@ -1,5 +1,5 @@
-use std::process::{Command, Output};
 use std::io::{Error, ErrorKind};
+use std::process::{Command, Output};
 
 struct SystemCommand {
     command: String,
@@ -27,13 +27,14 @@ impl SystemCommand {
     }
 
     fn execute(&self) -> Result<Output, Error> {
-        let output = Command::new(&self.command)
-            .args(&self.args)
-            .output();
+        let output = Command::new(&self.command).args(&self.args).output();
 
         match output {
             Ok(output) => Ok(output),
-            Err(e) => Err(Error::new(ErrorKind::Other, format!("Failed to execute command: {}", e))),
+            Err(e) => Err(Error::new(
+                ErrorKind::Other,
+                format!("Failed to execute command: {}", e),
+            )),
         }
     }
 
@@ -76,25 +77,45 @@ impl SystemCommand {
 }
 
 pub fn system_command_test() {
-    let mut ls_command = SystemCommand::new("git");
-    ls_command.arg("status").arg("-s");
 
-    match ls_command.execute_and_get_stdout() {
-        Ok(output) => println!("Output:\n{}", output),
+    let mut git_status_s = SystemCommand::new("git");
+    git_status_s.arg("status").arg("-s");
+
+    match git_status_s.execute_and_get_stdout() {
+        Ok(output) => println!("GitStatusS:\n{}", output),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+    let mut git_diff = SystemCommand::new("git");
+    git_diff.arg("diff");
+
+    match git_diff.execute_and_get_stdout() {
+        Ok(output) => println!("GitDiff:\n{}", output),
         Err(e) => eprintln!("Error: {}", e),
     }
 
-    let mut grep_command = SystemCommand::new("grep");
-    grep_command.args(&["-i", "test"]);
+    //let mut grep_command = SystemCommand::new("cat");
+    //grep_command.args(&["README.md"]);
+    //match grep_command.execute_and_get_stdout() {
+    //    Ok(output) => println!("CatREADME_md:\n{}", output),
+    //    Err(e) => eprintln!("Error: {}", e),
+    //}
+
+
+
 
     let test_string = "This is a Test string\nAnother line with test";
 
     // You'd typically pipe the output of another command into grep.
     // However, for this example, we'll simulate the input by writing to stdin.
-    use std::process::{Stdio};
     use std::io::Write;
+    use std::process::Stdio;
 
-    let mut grep_process = Command::new("grep").args(&["-i", "test"]).stdin(Stdio::piped()).stdout(Stdio::piped()).spawn().unwrap();
+    let mut grep_process = Command::new("grep")
+        .args(&["-i", "test"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
 
     if let Some(mut stdin) = grep_process.stdin.take() {
         stdin.write_all(test_string.as_bytes()).unwrap();
@@ -103,20 +124,23 @@ pub fn system_command_test() {
     let grep_output = grep_process.wait_with_output().unwrap();
 
     if grep_output.status.success() {
-        println!("Grep output: {}", String::from_utf8_lossy(&grep_output.stdout));
+        println!(
+            "Grep output: {}",
+            String::from_utf8_lossy(&grep_output.stdout)
+        );
     } else {
         println!("Grep failed");
     }
 
-    let mut false_command = SystemCommand::new("false");
-    match false_command.execute_and_check_status() {
-        Ok(status) => println!("False command status: {}", status),
-        Err(e) => eprintln!("Error: {}", e),
-    }
+    //let mut false_command = SystemCommand::new("false");
+    //match false_command.execute_and_check_status() {
+    //    Ok(status) => println!("False command status: {}", status),
+    //    Err(e) => eprintln!("Error: {}", e),
+    //}
 
-    let mut error_command = SystemCommand::new("nonexistent_command");
-    match error_command.execute_and_get_stdout() {
-        Ok(output) => println!("Output: {}", output),
-        Err(e) => eprintln!("Error: {}", e),
-    }
+    //let mut error_command = SystemCommand::new("nonexistent_command");
+    //match error_command.execute_and_get_stdout() {
+    //    Ok(output) => println!("Output: {}", output),
+    //    Err(e) => eprintln!("Error: {}", e),
+    //}
 }
